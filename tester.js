135be659f1428b205cbd22f3a7c01c54f4e9689c
Ashwita3 <ap6347@nyu.edu>
2020-02-24 17:39:10 -0500

    the second commit

--- tester.js
+++ tester.js
@@ -8,7 +8,7 @@ app.appendChild(container)
 var request = new XMLHttpRequest()
 
 request.open('GET', 'http://hp-api.herokuapp.com/api/characters', true)
-request.open('GET', 'http://hp-api.herokuapp.com/api/characters/students', true)
+// request.open('GET', 'http://hp-api.herokuapp.com/api/characters/students', true)
 
 request.onload = function(){
     var data = JSON.parse(this.response)
@@ -30,10 +30,35 @@ request.onload = function(){
             charImage.setAttribute('class','charImage')
             charImage.src = character.image;
 
+            //to show hp characters Species eg. if human, half-gaint
+            const charSpecies = document.createElement('p')
+            charSpecies.setAttribute('class','charSpecies')
+            charSpecies.textContent = character.species;
+
+            //to show hp characters House
+            const charHouse = document.createElement('p')
+            charHouse.setAttribute('class','charHouses')
+            charHouse.textContent = character.house;
+
+            //to show hp characters Ancestry
+            const charAncestry = document.createElement('p')
+            charAncestry.setAttribute('class','charAncestry')
+            charAncestry.textContent = character.ancestry;
+
+           
+            //to show hp characters year of birth
+            const charBirth = document.createElement('p')
+            charBirth.setAttribute('class','charBirth')
+            charBirth.textContent = character.yearOfBirth;
+
             //finally appending all of these details into the card
             container.appendChild(card)
             card.appendChild(charName)
             card.appendChild(charImage)
+            card.appendChild(charSpecies)
+            card.appendChild(charHouse)
+            card.appendChild(charAncestry)
+            card.appendChild(charBirth)
         })
     }else{
         const errorMessage = document.createElement('marquee')
